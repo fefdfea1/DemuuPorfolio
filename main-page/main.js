bff07040e3abc98e03e906e8f7ed9f87dae84ce1
demuu <preasim.kr@gmail.com>
2023-06-27 21:18:09 +0900

    portfolio

--- main-page/main.js
+++ main-page/main.js
@@ -1,10 +1,12 @@
 import { initCaroselUl } from "./slideAnimation.js";
 import {downArrow } from "./downArrow.js";
 import {portfolioLogo} from "./portfolioSVGLogo.js";
+import {scrollAction} from "./scrollAction.js";
 
 const init = () => {
   initCaroselUl();
   downArrow();
 	portfolioLogo();
+  scrollAction();
 };
 init();
