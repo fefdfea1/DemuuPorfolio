7e4982743595f26395dca7fb4d0618359c638f11
demuu <preasim.kr@gmail.com>
2023-07-01 05:34:21 +0900

    portfolio

--- main-page/scrollAction.js
+++ main-page/scrollAction.js
@@ -1,44 +1,60 @@
-function scroll () {
+import { section2Animation } from "./section2Js/section2Entry.js";
+
+let timer;
+function scroll() {
   window.onload = () => {
-    const elm = document.querySelectorAll('.section');
+    const elm = document.querySelectorAll(".section");
     const elmCount = elm.length;
+    let sectionPosition = 0;
+    
     elm.forEach((item, index) => {
-      item.addEventListener('mousewheel', (event) => {
+      item.addEventListener("mousewheel", (event) => {
         event.preventDefault();
-        let delta = 0;
-  
-        if (!event) event = window.event;
-        if (event.wheelDelta) {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          let delta = 0;
+
+          if (!event) event = window.event;
+          if (event.wheelDelta) {
             delta = event.wheelDelta / 120;
             if (window.opera) delta = -delta;
-        } 
-        else if (event.detail)
-            delta = -event.detail / 3;
-  
-        let moveTop = window.scrollY;
-        let elmSelector = elm[index];
+          } else if (event.detail) delta = -event.detail / 3;
 
-        // wheel down : move to next section
-        if (delta < 0){
-          if (elmSelector !== elmCount-1){
-            try{
-              moveTop = window.scrollY + elmSelector.nextElementSibling.getBoundingClientRect().top;
-            }catch(e){}
+          let moveTop = window.scrollY;
+          let elmSelector = elm[index];
+
+          // wheel down : move to next section
+          if (delta < 0) {
+            if (elmSelector !== elmCount - 1) {
+              try {
+                moveTop =
+                  window.scrollY +
+                  elmSelector.nextElementSibling.getBoundingClientRect().top;
+                  sectionPosition = Math.min(sectionPosition+1, elmCount - 1);
+              } catch (e) {}
+            }
           }
-        }
-        // wheel up : move to previous section
-        else{
-          if (elmSelector !== 0){
-            try{
-              moveTop = window.scrollY + elmSelector.previousElementSibling.getBoundingClientRect().top;
-            }catch(e){}
+          // wheel up : move to previous section
+          else {
+            if (elmSelector !== 0) {
+              try {
+                moveTop =
+                  window.scrollY +
+                  elmSelector.previousElementSibling.getBoundingClientRect().top;
+                  sectionPosition = Math.max(sectionPosition - 1, 0);
+              } catch (e) {}
+            }
           }
-        }
-  
-        window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+          
+          section2Animation(sectionPosition);
+          window.scrollTo({ top: moveTop, left: 0, behavior: "smooth" });
+          //마우스 인식 시간 설정
+        }, 200);
       });
     });
-  }
+  };
 }
 
 export const scrollAction = () => {
--- main-page/section2Js/section2Entry.js
+++ main-page/section2Js/section2Entry.js
@@ -1,3 +1,15 @@
-export const section2Animation = (elmSelector, elmCount) => {
-
+export const section2Animation = (sectionPosition) => {
+  const portfolioLogo = document.querySelector('.portfolio-logo');
+  const portfolioName = document.querySelector('.portfolio-logo-name');
+  const triangle = document.querySelector('.triangle');
+  
+  if (sectionPosition == 1) {
+    setTimeout(() => {
+      triangle.style.display = 'block';
+      portfolioLogo.style.width = '45%';
+      setTimeout(() => {
+        portfolioName.style.opacity = '1';
+      }, 700)
+    }, 2000)
+  }
 }
